88aa3d1e2d0d11b199722cd6017ef0f113be615d
ssilvestri15 <Ss112123!>
2023-02-06 18:37:58 +0100

    Provato a decryptare

--- src/main/java/controls/AudioControl.java
+++ src/main/java/controls/AudioControl.java
@@ -1,11 +1,21 @@
 package controls;
 
+import com.macasaet.fernet.Key;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.json.simple.parser.JSONParser;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.util.Base64;
 
 @WebServlet(name = "AudioControl", value = "/audio-control")
 public class AudioControl extends HttpServlet {
@@ -16,11 +26,32 @@ public class AudioControl extends HttpServlet {
             Object obj;
             String idAudio = request.getParameter("idAudio");
             obj = ServerPY.run("/api/audio/play?audio_id="+ idAudio);
-            request.setAttribute("audio",obj);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(obj);
+            oos.flush();
+            byte[] byteArray = baos.toByteArray();
+
+            Security.addProvider(new BouncyCastleProvider());
+            String encodedString = "GUTdbvcDC4bJV0v5b2-H4zSyQde8AnHzG4zWgJ7ra9w=".replace("-","/");
+            byte[] key = Base64.getDecoder().decode(encodedString.getBytes("UTF-8"));
+            byte[] iv = new byte[16];
+            for (int i=9, z=0; i<25; i++, z++) {
+                iv[z] = byteArray[i];
+            }
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding","BC");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
+            byte[] decryptedData = cipher.doFinal(byteArray);
+            request.setAttribute("audio",decryptedData);
+
         }catch (Exception e){
             e.printStackTrace();
         }
-        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("audio.jsp");
+
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/provola.jsp");
         dispatcher.forward(request, response);
 
     }
---
+++ src/main/webapp/provola.jsp
@@ -0,0 +1,29 @@
+<%@ page import="java.util.Base64" %>
+<%@ page import="java.io.ByteArrayOutputStream" %>
+<%@ page import="java.io.ObjectOutputStream" %><%--
+  Created by IntelliJ IDEA.
+  User: silve
+  Date: 06/02/2023
+  Time: 16:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+    byte[] file = (byte[]) request.getAttribute("audio");
+    if(file == null) {
+        response.sendRedirect("audio-control");
+    } else {
+%>
+<html>
+<head>
+    <title>Audio</title>
+</head>
+<body>
+<audio controls>
+    <source src=<%="data:audio/wav;base64," + Base64.getEncoder().encodeToString(file)%> type="audio/wav">
+    Your browser does not support the audio element.
+</audio>
+
+</body>
+</html>
+<% } %>
